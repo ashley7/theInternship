f15b6b0ef2af36657d58f3dc5c7e5cec97d8c6bd
THEMBO CHARLES LWANGA <ashley7520charles@gmail.com>
2021-07-02 10:55:12 +0300

    Removes all the code

--- index.php
+++ index.php
@@ -1,15 +1,1 @@
-<?php
-
-require("master.php");
-
-$persons = [
-
-	'Charles' => 2000,
-	'Thembo' => 2005,
-];
-
-foreach ($persons as $key => $value) {
-
-	echo "<br>". computeYear($key,$value);
-	 
-} 
+ 
