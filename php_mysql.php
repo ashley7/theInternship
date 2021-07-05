fd7dc3adf67889bf03719750e46c7ee3afea408b
THEMBO CHARLES LWANGA <ashley7520charles@gmail.com>
2021-07-05 12:19:32 +0300

    Adds the database creation commands

--- php_mysql.php
+++ php_mysql.php
@@ -1,2 +1,9 @@
 <?php
-$connection = new mysqli("localhost","root","e+-@MNZNNNckyy8PCgTAq+rTXp]zx}","internship");
+$connection = new mysqli("localhost","root","e+-@MNZNNNckyy8PCgTAq+rTXp]zx}");
+
+$connection->query("CREATE DATABASE IF NOT EXISTS internship");
+
+mysqli_select_db($connection,"internship");
+
+$connection->query("CREATE TABLE IF NOT EXISTS users(ID INT(11) NOT NULL AUTO_INCREMENT, PRIMARY KEY(ID), NAME VARCHAR(50) NOT NULL, PHONE_NUMBER VARCHAR(20) NOT NULL UNIQUE)");
+
