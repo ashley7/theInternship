ee9e90f10736b17d59273c7d3d483d15cfcb81f1
Ashley7 <ashley7520charles@gmail.com>
2021-07-14 10:41:59 +0300

    Working with USSD

--- ussd/ussd.php
+++ ussd/ussd.php
@@ -1,26 +1,14 @@
 <?php
-header("Content-type: text/plane");
+header("Content-type:text/plane");
 
-$text = $_POST["text"];
+$phone_number = $_POST['phoneNumber'];
 
-$phone_number = $_POST["phoneNumber"];
+$textFromUser = $_POST['text'];
 
-$session = $_POST["sessionId"];
+$sessionID = $_POST['sessionId'];
 
-$serviceCode = $_POST["serviceCode"];
+$serviceCode = $_POST['serviceCode'];
 
-if(empty($text)){
-
-	$response = "CON Welcome to the Climate change system";
-
-	$response .= "\n1.Register as a member";
-	$response .= "\n2.Add Tree";
-	$response .= "\n3.Close";
-
-	echo $response;	 
-
-}
-
-
+echo "CON Welcome to the Climate portal";
 
 
