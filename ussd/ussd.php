182e4e7e9b07f8e9feb12ae4b5187784401e9b39
Ashley7 <ashley7520charles@gmail.com>
2021-07-14 11:00:03 +0300

    Sending the welcome message

--- ussd/ussd.php
+++ ussd/ussd.php
@@ -9,6 +9,46 @@ $sessionID = $_POST['sessionId'];
 
 $serviceCode = $_POST['serviceCode'];
 
-echo "Welcome to the Climate portal";
+$inputArray = explode("*", $textFromUser);
+
+if (empty($inputArray)) {
+
+	$response = "CON Welcome to the Climate (U) Limited";
+
+	$response .= "\n 1. Register";
+
+	$response .= "\n 2. Add a tree";
+
+	echo $response;
+	 
+}
+
+
+
+
+
+ 
+
+
+
+
+
+
+// Welcome message 
+
+// Choose an option 
+
+//   1. Register 
+//   2. Add a tree 
+//   3. Council 
+
+// 1.  WHat is your name?
+//     send them sms that they have registered 
+
+// 2.  Check id they have registered 
+//     Enter number of of trees 
+//      Send a thank you message
+
+
 
 
